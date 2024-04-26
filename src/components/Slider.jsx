ea12200325b4cffe780e92fccd77649f045fe333
Asish Binu Mathew <asishbm98@gmail.com>
2024-04-26 14:52:34 +0100

    Themed

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -43,6 +43,35 @@ export default function RangeSlider(props) {
   return (
     <Box sx={{ width: 500 }}>
       <Slider
+        sx={{
+          color: '#3a8589',
+          height: 3,
+          padding: '13px 0',
+          '& .MuiSlider-thumb': {
+            height: 27,
+            width: 27,
+            backgroundColor: '#fff',
+            border: '1px solid currentColor',
+            '&:hover': {
+              boxShadow: '0 0 0 8px rgba(58, 133, 137, 0.16)',
+            },
+            '& .airbnb-bar': {
+              height: 9,
+              width: 1,
+              backgroundColor: 'currentColor',
+              marginLeft: 1,
+              marginRight: 1,
+            },
+          },
+          '& .MuiSlider-track': {
+            height: 3,
+          },
+          '& .MuiSlider-rail': {
+            color: '#d8d8d8',
+            opacity: 1,
+            height: 3,
+          },
+        }}
         getAriaLabel={() => 'Minimum distance'}
         value={props.value1}
         scale={(x) => {
